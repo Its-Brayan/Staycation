32daf611841defe8af340b8ad50eca2e96b64155
Brayan <brayanmwangi732@gmail.com>
2025-08-11 21:51:54 +0300

    Favourite page ui

--- lib/pages/favourite.dart
+++ lib/pages/favourite.dart
@@ -10,8 +10,37 @@ class Favourite extends StatefulWidget {
 class _FavouriteState extends State<Favourite> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Favourites page"),
+    return DefaultTabController(
+      length:2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text("Favorites",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF01226F)
+          ),),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(text: "Hotels",
+            ),
+            Tab(text:"Article")
+          ]),
+        ),
+        
+        body: TabBarView(
+          children: [
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              child: Text("Hi"),
+            )
+          ],
+
+
+          ),
+        ),
     );
   }
 }

e90ac66b297aeb2ee810f8f29fdc5c42b97015f2
Brayan <brayanmwangi732@gmail.com>
2025-08-12 08:48:55 +0300

    backend setup

---
+++ staybackend/myworld/my_stay_cation/my_stay_cation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for my_stay_cation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_stay_cation.settings')
+
+application = get_asgi_application()
